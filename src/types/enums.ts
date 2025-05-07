6ab75c27bf23fd322a30c68bbac45fc04b7060b5
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-05-07 02:07:03 +0200

    feat: add input-builder utility class

---
+++ src/types/enums.ts
@@ -0,0 +1,11 @@
+export enum INPUT_TYPE {
+  TEXT = 'text',
+  NUMBER = 'number',
+  PASSWORD = 'password',
+  DATE = 'date',
+  CHECKBOX = 'checkbox',
+  RADIO = 'radio',
+  SEARCH = 'search',
+  TEL = 'tel',
+  EMAIL = 'email',
+}
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,3 +1,5 @@
+import type { INPUT_TYPE } from './enums';
+
 export interface ElementParameters {
   tag: string;
   className: string | string[];
@@ -5,4 +7,11 @@ export interface ElementParameters {
   callback?: (event: Event) => void;
   eventType?: string;
   attributes?: Record<string, string>;
+}
+
+export interface InputParameters extends ElementParameters {
+  type: INPUT_TYPE;
+  value?: string;
+  placeholder?: string;
+  readonly?: boolean;
 }
