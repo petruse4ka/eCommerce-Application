df7ba34e8772b08c03b103d29e00bc5c5500d12c
pokolga <pokolga1801@gmail.com>
2025-05-07 21:55:21 +0300

    feat: add password validate and test for it

--- src/tests/validate.test.ts
+++ src/tests/validate.test.ts
@@ -1,4 +1,4 @@
-import { validateEMail } from '@/utils/validate';
+import { validateEMail, validatePassword } from '@/utils/validate';
 
 it('email validation of rigth Latin input', () => {
   expect(validateEMail('example@tut.by')).toEqual(null);
@@ -19,7 +19,6 @@ it('email validation of short input', () => {
 it('email validation of double domain', () => {
   expect(validateEMail('info@company.com.by')).toEqual(null);
 });
-
 it('email validation of empty input', () => {
   expect(validateEMail('')).toBe('Field must be filled in');
 });
@@ -47,4 +46,44 @@ it('email validation with wrong characters', () => {
   expect(validateEMail('name@dom!ain.com')).toBe(
     'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.'
   );
+});
+
+it('password validation of empty input', () => {
+  expect(validatePassword('')).toBe(
+    'Password must contain at least: one upper case letter, one lower case letter, one number. It`s length must be 8 characters and more.'
+  );
+});
+it('password validation of nonLatinic input', () => {
+  expect(validatePassword('пароль')).toBe(
+    'Password must contain only Latin and special characters and numbers'
+  );
+});
+it('password validation for password without upper character', () => {
+  expect(validatePassword('a1234567')).toBe(
+    'Password must contain at least: one upper case letter'
+  );
+});
+it('password validation for password without lower character', () => {
+  expect(validatePassword('A1234567')).toBe(
+    'Password must contain at least: one lower case letter'
+  );
+});
+it('password validation for password without number', () => {
+  expect(validatePassword('ABCDEFabcdef')).toBe('Password must contain at least: one number');
+});
+it('password validation for password without upper character and number', () => {
+  expect(validatePassword('asdfghjk')).toBe(
+    'Password must contain at least: one upper case letter, one number'
+  );
+});
+it('password validation for length of password less then 8 characters', () => {
+  expect(validatePassword('aA8')).toBe('Password length must be 8 characters and more.');
+});
+it('password validation for all wrong parameters', () => {
+  expect(validatePassword('pass')).toBe(
+    'Password must contain at least: one upper case letter, one number. It`s length must be 8 characters and more.'
+  );
+});
+it('password validation for right password', () => {
+  expect(validatePassword('aA8asdf$#-')).toEqual(null);
 });
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -7,3 +7,23 @@ export function validateEMail(string_: string): string | null {
   if (emailRegex.test(string_)) return null;
   return 'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.';
 }
+
+export function validatePassword(string_: string): string | null {
+  const nonLatinRegex = /[^\d!#$%&*+.:=?@A-Za-z{}-]/;
+  if (nonLatinRegex.test(string_))
+    return 'Password must contain only Latin and special characters and numbers';
+
+  let result = '';
+  const resultArray = [];
+  if (!/[A-Z]/.test(string_)) resultArray.push('one upper case letter');
+  if (!/[a-z]/.test(string_)) resultArray.push('one lower case letter');
+  if (!/\d/.test(string_)) resultArray.push('one number');
+
+  if (resultArray.length > 0) result = 'Password must contain at least: ' + resultArray.join(', ');
+  const minLengthOfPassword = 8;
+  if (string_.trim().length < minLengthOfPassword) {
+    const prefix = (result.length > 0 && '. It`s') || 'Password';
+    result += `${prefix} length must be ${minLengthOfPassword} characters and more.`;
+  }
+  return result || null;
+}
