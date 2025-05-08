5da73b337862fbda32f30c2b944f16f2fcd6f5d2
pokolga <pokolga1801@gmail.com>
2025-05-08 12:58:01 +0300

    fix: russification of error messages

--- src/tests/validate.test.ts
+++ src/tests/validate.test.ts
@@ -20,68 +20,68 @@ it('email validation of double domain', () => {
   expect(validateEMail('info@company.com.by')).toEqual(null);
 });
 it('email validation of empty input', () => {
-  expect(validateEMail('')).toBe('Field must be filled in');
+  expect(validateEMail('')).toBe('Поле обязательно к заполнению.');
 });
 it('email validation of input without @', () => {
   expect(validateEMail('exampletut.by')).toBe(
-    'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.'
+    'Неверный формат email! Допускаются латиница и кириллица, цифры, специальные символы. Знак «@» обязателен!'
   );
 });
 it('email validation of invalid input', () => {
   expect(validateEMail('example@tut,by')).toBe(
-    'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.'
+    'Неверный формат email! Допускаются латиница и кириллица, цифры, специальные символы. Знак «@» обязателен!'
   );
 });
 it('email validation of beginning with dot', () => {
   expect(validateEMail('.user@mail.com')).toBe(
-    'Incorrect email format!  The first character must be a letter or a number.'
+    'Неверный формат email! Первым символом должна быть буква или цифра.'
   );
 });
 it('email validation without domain', () => {
   expect(validateEMail('name@domain')).toBe(
-    'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.'
+    'Неверный формат email! Допускаются латиница и кириллица, цифры, специальные символы. Знак «@» обязателен!'
   );
 });
 it('email validation with wrong characters', () => {
   expect(validateEMail('name@dom!ain.com')).toBe(
-    'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.'
+    'Неверный формат email! Допускаются латиница и кириллица, цифры, специальные символы. Знак «@» обязателен!'
   );
 });
 
 it('password validation of empty input', () => {
   expect(validatePassword('')).toBe(
-    'Password must contain at least: one upper case letter, one lower case letter, one number. It`s length must be 8 characters and more.'
+    'Пароль должен содержать как минимум: одну заглавную букву, одну строчную букву, одну цифру. Его длина должна быть минимум 8 символов.'
   );
 });
 it('password validation of nonLatinic input', () => {
   expect(validatePassword('пароль')).toBe(
-    'Password must contain only Latin and special characters and numbers'
+    'Пароль может содержать только цифры, латинские и специальные символы.'
   );
 });
 it('password validation for password without upper character', () => {
   expect(validatePassword('a1234567')).toBe(
-    'Password must contain at least: one upper case letter'
+    'Пароль должен содержать как минимум: одну заглавную букву'
   );
 });
 it('password validation for password without lower character', () => {
   expect(validatePassword('A1234567')).toBe(
-    'Password must contain at least: one lower case letter'
+    'Пароль должен содержать как минимум: одну строчную букву'
   );
 });
 it('password validation for password without number', () => {
-  expect(validatePassword('ABCDEFabcdef')).toBe('Password must contain at least: one number');
+  expect(validatePassword('ABCDEFabcdef')).toBe('Пароль должен содержать как минимум: одну цифру');
 });
 it('password validation for password without upper character and number', () => {
   expect(validatePassword('asdfghjk')).toBe(
-    'Password must contain at least: one upper case letter, one number'
+    'Пароль должен содержать как минимум: одну заглавную букву, одну цифру'
   );
 });
 it('password validation for length of password less then 8 characters', () => {
-  expect(validatePassword('aA8')).toBe('Password length must be 8 characters and more.');
+  expect(validatePassword('aA8')).toBe('Длина пароля  должна быть минимум 8 символов.');
 });
 it('password validation for all wrong parameters', () => {
   expect(validatePassword('pass')).toBe(
-    'Password must contain at least: one upper case letter, one number. It`s length must be 8 characters and more.'
+    'Пароль должен содержать как минимум: одну заглавную букву, одну цифру. Его длина должна быть минимум 8 символов.'
   );
 });
 it('password validation for right password', () => {
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,29 +1,60 @@
 export function validateEMail(string_: string): string | null {
-  if (string_.trim() === '') return 'Field must be filled in';
+  if (string_.trim() === '') return 'Поле обязательно к заполнению.';
   if (/[^\dA-Za-zЁА-яё]/.test(string_[0]))
-    return 'Incorrect email format!  The first character must be a letter or a number.';
+    return 'Неверный формат email! Первым символом должна быть буква или цифра.';
   const emailRegex =
     /^[\dA-Za-zЁА-яё][\w!#$%&*+./=?^`{|}~ЁА-яё’-]*@[\d.A-Za-zЁА-яё-]+\.[A-Za-zЁА-яё]{2,}$/;
   if (emailRegex.test(string_)) return null;
-  return 'Incorrect email format! Latin and Cyrillic characters, numbers, special characters and the obligatory "@" sign are allowed.';
+  return 'Неверный формат email! Допускаются латиница и кириллица, цифры, специальные символы. Знак «@» обязателен!';
 }
 
 export function validatePassword(string_: string): string | null {
   const nonLatinRegex = /[^\d!#$%&*+.:=?@A-Za-z{}-]/;
   if (nonLatinRegex.test(string_))
-    return 'Password must contain only Latin and special characters and numbers';
+    return 'Пароль может содержать только цифры, латинские и специальные символы.';
 
   let result = '';
   const resultArray = [];
-  if (!/[A-Z]/.test(string_)) resultArray.push('one upper case letter');
-  if (!/[a-z]/.test(string_)) resultArray.push('one lower case letter');
-  if (!/\d/.test(string_)) resultArray.push('one number');
+  if (!/[A-Z]/.test(string_)) resultArray.push('одну заглавную букву');
+  if (!/[a-z]/.test(string_)) resultArray.push('одну строчную букву');
+  if (!/\d/.test(string_)) resultArray.push('одну цифру');
 
-  if (resultArray.length > 0) result = 'Password must contain at least: ' + resultArray.join(', ');
+  if (resultArray.length > 0)
+    result = 'Пароль должен содержать как минимум: ' + resultArray.join(', ');
   const minLengthOfPassword = 8;
   if (string_.trim().length < minLengthOfPassword) {
-    const prefix = (result.length > 0 && '. It`s') || 'Password';
-    result += `${prefix} length must be ${minLengthOfPassword} characters and more.`;
+    const prefix = (result.length > 0 && '. Его длина') || 'Длина пароля ';
+    result += `${prefix} должна быть минимум ${minLengthOfPassword} символов.`;
   }
   return result || null;
+}
+
+export function validateDateOfBirth(dateOfBirth: string): string | null {
+  const birthDate = new Date(dateOfBirth);
+  if (Number.isNaN(birthDate.getTime())) {
+    return 'Формат даты: ГГГГ-ММ-ДД';
+  }
+  const userAge = getUserAge(birthDate);
+  let result = '';
+  if (userAge < 0 || userAge > 130) {
+    result = 'Проверьте, как введен год рождения. ';
+  } else if (userAge < 13) {
+    result = 'Допустимый возраст пользователя - старше 13 лет.';
+  } else {
+    return null;
+  }
+  return result;
+}
+
+function getUserAge(birthDate: Date): number {
+  const today = new Date();
+  const age = today.getFullYear() - birthDate.getFullYear();
+
+  if (
+    today.getMonth() < birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
+  ) {
+    return age - 1;
+  }
+  return age;
 }
