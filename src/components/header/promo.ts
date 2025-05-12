4733139688cd5417617e659e5a1fb7f0e7a4d16e
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-05-12 18:16:30 +0200

    refactor: move promo items object into data file

--- src/components/header/promo.ts
+++ src/components/header/promo.ts
@@ -1,9 +1,5 @@
-import fastDeliveryIcon from '@/assets/icons/fast-delivery.svg';
-import freshProductsIcon from '@/assets/icons/fresh-products.svg';
-import naturalIngredients from '@/assets/icons/natural-ingredients.svg';
-import wholesaleSale from '@/assets/icons/wholesale-offer.svg';
 import { BaseComponent } from '@/components/base/component';
-import { SUBHEADER_PROMO_TEXT } from '@/constants/constants';
+import { PROMO_ITEMS } from '@/data';
 import { SUBHEADER_STYLES } from '@/styles/header/subheader';
 import { ElementBuilder } from '@/utils/element-builder';
 import { ImageBuilder } from '@/utils/image-builder';
@@ -15,14 +11,7 @@ export default class Promo extends BaseComponent {
   }
 
   protected render(): void {
-    const promoItems = [
-      { icon: fastDeliveryIcon, text: SUBHEADER_PROMO_TEXT.DELIVERY },
-      { icon: freshProductsIcon, text: SUBHEADER_PROMO_TEXT.FRESH },
-      { icon: wholesaleSale, text: SUBHEADER_PROMO_TEXT.WHOLESALE },
-      { icon: naturalIngredients, text: SUBHEADER_PROMO_TEXT.INGREDIENTS },
-    ];
-
-    for (const item of promoItems) {
+    for (const item of PROMO_ITEMS) {
       const promoItem = new ElementBuilder({
         tag: 'div',
         className: SUBHEADER_STYLES.PROMO_ITEM,
