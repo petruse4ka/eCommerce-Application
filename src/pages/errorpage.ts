de70d5fc01a5218a76e6d918b3de186aca3301dd
pokolga <pokolga1801@gmail.com>
2025-05-13 17:42:43 +0300

    feat: add button to return to main page

--- src/pages/errorpage.ts
+++ src/pages/errorpage.ts
@@ -3,10 +3,9 @@ import '@/styles/main.css';
 import crumbImg from '@/assets/images/error-crumb.png';
 import macaronImg from '@/assets/images/error-macaron.png';
 import { BaseComponent } from '@/components/base/component';
+import { Button } from '@/components/buttons/button';
 import { ElementBuilder } from '@/utils/element-builder';
 import { ImageBuilder } from '@/utils/image-builder';
-
-const CRUMB_STYLE = ['absolute', 'top-1/2', 'left-1/2'];
 
 export class ErrorPage extends BaseComponent {
   constructor() {
@@ -28,36 +27,56 @@ export class ErrorPage extends BaseComponent {
   }
 
   private render(): void {
+    this.getTitle();
+
+    const macaron = new ImageBuilder({
+      className: ['absolute', 'top-1/3', 'left-1/2', '-translate-x-1/2'],
+      source: macaronImg,
+      alt: 'big sad macaron',
+    }).getElement();
+    this.component.append(macaron);
+
+    this.getCrumbImage(['translate-x-1/2', 'mt-16', 'transform', 'origin-center', 'animate-spin']);
+    this.getCrumbImage(['-translate-x-full', '-ml-16']);
+
+    const buttonContainer = new ElementBuilder({
+      tag: 'div',
+      className: ['flex', 'justify-center', 'items-end', 'h-{80vh}'],
+    }).getElement();
+
+    const returnButton = new Button({
+      style: 'SECONDARY_BLUE',
+      textContent: 'На главную',
+      callback: (): void => {},
+    }).getElement();
+
+    buttonContainer.append(returnButton);
+
+    this.component.append(buttonContainer);
+  }
+
+  private getTitle(): void {
     const title = new ElementBuilder({
       tag: 'h1',
       className: ['text-2xl', 'text-center', 'font-montserrat', 'font-bold', 'p-4'],
       textContent: 'Извините, страница не найдена',
     }).getElement();
+    if (!(title instanceof HTMLHeadingElement)) {
+      throw new TypeError('The element is not HTMLHeadingElement');
+    }
     this.component.append(title);
-    const macaron = new ImageBuilder({
-      className: ['absolute', 'top-1/3', 'left-1/2', '-translate-x-1/2'],
-      source: macaronImg,
-      alt: 'animated macaron',
-    }).getElement();
-    this.component.append(macaron);
-    const crumbRight = new ImageBuilder({
-      className: [
-        ...CRUMB_STYLE,
-        'translate-x-1/2',
-        'mt-16',
-        'transform',
-        'origin-center',
-        'animate-spin',
-      ],
-      source: crumbImg,
-      alt: 'animated macaron',
-    }).getElement();
-    this.component.append(crumbRight);
-    const crumbLeft = new ImageBuilder({
-      className: [...CRUMB_STYLE, '-translate-x-full', '-ml-16'],
+  }
+
+  private getCrumbImage(addClass: string[]): void {
+    const CRUMB_STYLE = ['absolute', 'top-1/2', 'left-1/2'];
+    const crumb = new ImageBuilder({
+      className: [...CRUMB_STYLE, ...addClass],
       source: crumbImg,
       alt: 'crumb',
     }).getElement();
-    this.component.append(crumbLeft);
+    if (!(crumb instanceof HTMLImageElement)) {
+      throw new TypeError('The element is not HTMLImageElement');
+    }
+    this.component.append(crumb);
   }
 }
