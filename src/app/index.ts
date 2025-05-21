923e27be439927feaba1ace70f1b71fac0e3585f
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-05-21 04:15:08 +0200

    feat: add catalog page with dummy content and set its routes

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,7 @@ import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
 import AboutPage from '@/pages/about';
 import AccountPage from '@/pages/account';
+import CatalogPage from '@/pages/catalog';
 import ContactsPage from '@/pages/contacts';
 import DeliveryPage from '@/pages/delivery';
 import ErrorPage from '@/pages/errorpage';
@@ -21,6 +22,7 @@ export default class App extends BaseComponent {
   private footer: Footer;
   private homePage: HomePage = new HomePage();
   private loginPage: LoginPage = new LoginPage();
+  private catalogPage: CatalogPage = new CatalogPage();
   private contactPage: ContactsPage = new ContactsPage();
   private aboutPage: AboutPage = new AboutPage();
   private registrationPage: RegistrationPage = new RegistrationPage();
@@ -43,6 +45,7 @@ export default class App extends BaseComponent {
     [Route.TERMS, this.termsPage],
     [Route.RETURNS, this.returnsPage],
     [Route.ACCOUNT, this.accountPage],
+    [Route.CATALOG, this.catalogPage],
   ]);
 
   constructor() {
