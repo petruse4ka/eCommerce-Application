96b924be9656622cc0de7897b144eda3c34f3c97
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-05-22 01:45:00 +0200

    feat: add burger menu close and open

--- src/components/header/main-menu.ts
+++ src/components/header/main-menu.ts
@@ -1,21 +1,39 @@
 import BaseComponent from '@/components/base';
 import { MENU_ITEMS } from '@/constants';
 import Router from '@/router';
+import { NOSCROLL } from '@/styles/app/app';
 import { HEADER_STYLES } from '@/styles/header/header';
 import ElementBuilder from '@/utils/element-builder';
 
 export default class MainMenu extends BaseComponent {
+  private isOpen: boolean;
+  private nav: ElementBuilder;
+  private burgerIconTop: ElementBuilder;
+  private burgerIconBottom: ElementBuilder;
+
   constructor() {
     super({ tag: 'div', className: HEADER_STYLES.MAIN_MENU });
+    this.isOpen = false;
+
+    this.nav = new ElementBuilder({
+      tag: 'nav',
+      className: HEADER_STYLES.MENU_NAV,
+    });
+
+    this.burgerIconTop = new ElementBuilder({
+      tag: 'span',
+      className: HEADER_STYLES.BURGER_ICON_TOP_CLOSED,
+    });
+
+    this.burgerIconBottom = new ElementBuilder({
+      tag: 'span',
+      className: HEADER_STYLES.BURGER_ICON_BOTTOM_CLOSED,
+    });
+
     this.render();
   }
 
   protected render(): void {
-    const nav = new ElementBuilder({
-      tag: 'nav',
-      className: HEADER_STYLES.MENU_NAV,
-    }).getElement();
-
     for (const item of MENU_ITEMS) {
       const menuItem = new ElementBuilder({
         tag: 'span',
@@ -24,29 +42,55 @@ export default class MainMenu extends BaseComponent {
       }).getElement();
 
       menuItem.addEventListener('click', () => {
+        this.isOpen = !this.isOpen;
+
+        if (this.isOpen) {
+          this.openMenu();
+        } else {
+          this.closeMenu();
+        }
+
         Router.followRoute(item.route);
       });
 
-      nav.append(menuItem);
+      this.nav.getElement().append(menuItem);
     }
 
     const burgerButton = new ElementBuilder({
       tag: 'button',
       className: HEADER_STYLES.BURGER_BUTTON,
+      callback: (): void => {
+        this.isOpen = !this.isOpen;
+        if (this.isOpen) {
+          this.openMenu();
+        } else {
+          this.closeMenu();
+        }
+      },
     }).getElement();
 
-    const burgerIconTop = new ElementBuilder({
-      tag: 'span',
-      className: HEADER_STYLES.BURGER_ICON_TOP,
-    }).getElement();
+    burgerButton.append(this.burgerIconTop.getElement(), this.burgerIconBottom.getElement());
 
-    const burgerIconTBottom = new ElementBuilder({
-      tag: 'span',
-      className: HEADER_STYLES.BURGER_ICON_BOTTOM,
-    }).getElement();
+    this.component.append(this.nav.getElement(), burgerButton);
+  }
 
-    burgerButton.append(burgerIconTop, burgerIconTBottom);
+  private openMenu(): void {
+    this.nav.removeCssClasses(HEADER_STYLES.MENU_NAV);
+    this.nav.applyCssClasses(HEADER_STYLES.MENU_NAV_OPEN);
+    this.burgerIconTop.removeCssClasses(HEADER_STYLES.BURGER_ICON_TOP_CLOSED);
+    this.burgerIconTop.applyCssClasses(HEADER_STYLES.BURGER_ICON_TOP_OPEN);
+    this.burgerIconBottom.removeCssClasses(HEADER_STYLES.BURGER_ICON_BOTTOM_CLOSED);
+    this.burgerIconBottom.applyCssClasses(HEADER_STYLES.BURGER_ICON_BOTTOM_OPEN);
+    document.body.classList.add(NOSCROLL.join(' '));
+  }
 
-    this.component.append(nav, burgerButton);
+  private closeMenu(): void {
+    this.nav.removeCssClasses(HEADER_STYLES.MENU_NAV_OPEN);
+    this.nav.applyCssClasses(HEADER_STYLES.MENU_NAV);
+    this.burgerIconTop.removeCssClasses(HEADER_STYLES.BURGER_ICON_TOP_OPEN);
+    this.burgerIconTop.applyCssClasses(HEADER_STYLES.BURGER_ICON_TOP_CLOSED);
+    this.burgerIconBottom.removeCssClasses(HEADER_STYLES.BURGER_ICON_BOTTOM_OPEN);
+    this.burgerIconBottom.applyCssClasses(HEADER_STYLES.BURGER_ICON_BOTTOM_CLOSED);
+    document.body.classList.remove(NOSCROLL.join(' '));
   }
 }
--- src/styles/app/app.ts
+++ src/styles/app/app.ts
@@ -1,1 +1,2 @@
 export const APP_STYLE = ['font-montserrat', 'flex', 'flex-col', 'min-h-screen'];
+export const NOSCROLL = ['overflow-hidden'];
