98cb3b1748c62b021a7fb7c2145738c1e140f330
tearzday <biverdaniil@gmail.com>
2025-05-26 14:55:42 +0300

    feat: add address-list component

---
+++ src/styles/address/index.ts
@@ -0,0 +1,34 @@
+export const ADDRESS = {
+  CONTAINER: ['border', 'border-accent', 'relative', 'rounded-md', 'px-4', 'py-10', 'hidden'],
+  TITLE: [
+    'absolute',
+    '-top-4',
+    'left-4',
+    'text-xl',
+    'font-bold',
+    'text-white',
+    'bg-accent',
+    'p-1',
+    'rounded-md',
+  ],
+  CARD: {
+    DEFAULT: ['rounded-md', 'p-2'],
+    DEFAULT_TITLE: [
+      'absolute',
+      '-top-4',
+      'left-1/2',
+      '-translate-x-1/2',
+      'font-bold',
+      'bg-primary',
+      'pt-1',
+      'px-1',
+      'rounded-md',
+    ],
+    ACTIVE: ['bg-primary', 'relative'],
+  },
+  LINE: {
+    CONTAINER: ['flex', 'gap-4', 'text-dark'],
+    TITLE: ['font-bold', 'text-xl'],
+    VALUE: ['text-xl'],
+  },
+};
