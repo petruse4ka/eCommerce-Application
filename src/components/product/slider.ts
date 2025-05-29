b33e8bc1392f362915ba0aeb6fb006ea5ac8463c
pokolga <pokolga1801@gmail.com>
2025-05-29 17:32:58 +0300

    feat: add slider adaptive layout

--- src/components/product/slider.ts
+++ src/components/product/slider.ts
@@ -1,7 +1,6 @@
-import setHeart from '@/assets/images/set-heart.png';
 import BaseComponent from '@/components/base';
 import { PRODUCT_STYLES } from '@/styles/pages/product';
-import ImageBuilder from '@/utils/image-builder';
+import ElementBuilder from '@/utils/element-builder';
 
 export default class ProductSlider extends BaseComponent {
   constructor() {
@@ -10,12 +9,39 @@ export default class ProductSlider extends BaseComponent {
   }
 
   protected render(): void {
-    const image = new ImageBuilder({
-      source: setHeart,
-      alt: 'Macaron set Heart',
-      className: PRODUCT_STYLES.IMAGE,
-    }).getElement();
+    const imageContainer = new ElementBuilder({
+      tag: 'div',
+      className: PRODUCT_STYLES.SLIDER_BIG_IMAGE,
+      callback: (): void => {},
+    });
 
-    this.component.append(image);
+    const array = [
+      '././assets/images/macarons/blue-cheese.png',
+      '././assets/images/dessert3.png',
+      '././assets/images/dessert1.png',
+      '././assets/images/macarons.jpg',
+    ];
+
+    imageContainer.applyCssClasses(`bg-[url('././assets/images/dessert1.png')]`);
+
+    this.component.append(imageContainer.getElement());
+    if (array.length > 1) {
+      const previewContainer = new ElementBuilder({
+        tag: 'div',
+        className: PRODUCT_STYLES.SLIDER_PREVIEW_CONTAINER,
+        callback: (): void => {},
+      }).getElement();
+      for (const image of array) {
+        const preview = new ElementBuilder({
+          tag: 'div',
+          className: PRODUCT_STYLES.SLIDER_PREVIEW,
+          callback: (): void => {},
+        });
+        preview.getElement().style.backgroundImage = `url('${image}')`;
+        //preview.applyCssClasses(`bg-[url('${image}')]`);
+        previewContainer.append(preview.getElement());
+      }
+      this.component.append(previewContainer);
+    }
   }
 }
