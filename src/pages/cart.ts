e6389ad035512813274964fb2fe75ad79bb233a8
tearzday <biverdaniil@gmail.com>
2025-06-07 15:52:23 +0300

    feat: add empty info for cart page

--- src/pages/cart.ts
+++ src/pages/cart.ts
@@ -1,9 +1,12 @@
 import '@/styles/main.css';
 
-import APICart from '@/api/cart';
+import notFoundImage from '@/assets/images/not-found.svg';
 import BaseComponent from '@/components/base';
+import EmptyComponent from '@/components/base/empty';
 import CartList from '@/components/cart/cart-list';
 import CartTotal from '@/components/cart/cart-total';
+import { CART_TEXT } from '@/constants';
+import { cartState } from '@/store/cart-state';
 import { userState } from '@/store/user-state';
 import { CART_PAGE } from '@/styles/pages/cart';
 import ElementBuilder from '@/utils/element-builder';
@@ -21,24 +24,40 @@ export default class CartPage extends BaseComponent {
       className: CART_PAGE.CONTAINER,
     }).getElement();
 
-    userState.subscribe(() => {
-      void this.render.bind(this);
-    });
-    void this.render();
+    userState.subscribe(this.render.bind(this));
 
-    this.component.append(this.container);
+    cartState.subscribe(this.render.bind(this));
+
+    this.render();
   }
 
-  private async render(): Promise<void> {
-    await APICart.getCart();
-
+  private render(): void {
     while (this.container.firstChild) {
       this.container.firstChild.remove();
     }
 
-    const cartList = new CartList();
-    const cartTotal = new CartTotal();
+    while (this.component.firstChild) {
+      this.component.firstChild.remove();
+    }
 
-    this.container.append(cartList.getElement(), cartTotal.getElement());
+    const total = cartState.getItemsCount();
+
+    if (total) {
+      const cartList = new CartList();
+      const cartTotal = new CartTotal();
+
+      this.container.append(cartList.getElement(), cartTotal.getElement());
+      this.component.append(this.container);
+    } else {
+      const emptyState = new EmptyComponent(
+        CART_TEXT.EMPTY,
+        notFoundImage,
+        CART_PAGE.EMPTY_PRODUCT_CONTAINER,
+        CART_PAGE.EMPTY_PRODUCT_IMAGE,
+        CART_PAGE.EMPTY_PRODUCT_TEXT
+      ).getElement();
+
+      this.component.append(emptyState);
+    }
   }
 }
